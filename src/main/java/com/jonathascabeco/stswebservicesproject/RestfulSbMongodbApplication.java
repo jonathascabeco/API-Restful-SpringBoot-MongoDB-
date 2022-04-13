bff2a5c9c1b20f85146aa3cdca09da276c6eb984
jonathascabeco <jonathascabeco@gmail.com>
2022-04-13 19:27:14 -0300

    First commit

---
+++ src/main/java/com/jonathascabeco/stswebservicesproject/RestfulSbMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.jonathascabeco.stswebservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSbMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSbMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathascabeco/stswebservicesproject/RestfulSbMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathascabeco.stswebservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulSbMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
